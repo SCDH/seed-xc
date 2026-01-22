a531bc766d7983bc861a8c6e6847caefac4a6937
Christian Lück <christian.lueck@uni-muenster.de>
2026-01-22 17:41:51 +0100

    adds the configuration exception
    Code copied from SEED XML Transformer

---
+++ api/src/main/java/de/ulbms/scdh/seed/xc/api/ConfigurationException.java
@@ -0,0 +1,25 @@
+package de.ulbms.scdh.seed.xc.api;
+
+/**
+ * An exception that occurred in one of the following phases:
+ *
+ * 1) during the configuration of the service, e.g. when reading the
+ * configuration file.
+ *
+ * 2) during the compilation of any of the transformations.
+ *
+ * Since service configuration AND compilation of all transformations
+ * take place before processing the first request, we use the same
+ * exception class for these kinds of errors.
+ */
+public class ConfigurationException extends Exception {
+    public ConfigurationException(String msg) {
+        super(msg);
+    }
+    public ConfigurationException(Throwable cause) {
+        super(cause);
+    }
+    public ConfigurationException(String msg, Throwable cause) {
+	super(msg, cause);
+    }
+}
