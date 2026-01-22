8636432ec874a5ebb18d0f1e867aea8cec399f2e
Christian Lück <christian.lueck@uni-muenster.de>
2026-01-22 13:15:14 +0100

    bootstrap Quarkus REST project with maven archetype
    mvn io.quarkus.platform:quarkus-maven-plugin:3.30.6:create \
        -DprojectGroupId=de.ulbms.scdh.seed.xc \
        -DprojectArtifactId=SEEDXC \
        -Dextensions='rest,quarkus-smallrye-openapi,quarkus-swagger-ui'

---
+++ service/src/main/java/de/ulbms/scdh/seed/xc/GreetingResource.java
@@ -0,0 +1,16 @@
+package de.ulbms.scdh.seed.xc;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
