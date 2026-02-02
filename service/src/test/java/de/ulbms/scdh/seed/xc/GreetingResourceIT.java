d7631e7cde70800c069fd0c7b5b60cfcfe2f5957
Christian Lück <christian.lueck@uni-muenster.de>
2026-02-02 17:22:03 +0100

    removes quarkus REST example generated by archetype

--- service/src/test/java/de/ulbms/scdh/seed/xc/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package de.ulbms.scdh.seed.xc;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
