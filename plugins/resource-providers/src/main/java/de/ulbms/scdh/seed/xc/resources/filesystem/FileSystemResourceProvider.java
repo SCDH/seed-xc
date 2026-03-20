2e92176f8375c048a18d5c9e1690617fb0a563e8
Christian Lück <christian.lueck@uni-muenster.de>
2026-03-20 22:31:46 +0100

    adds file system resource provider

---
+++ plugins/resource-providers/src/main/java/de/ulbms/scdh/seed/xc/resources/filesystem/FileSystemResourceProvider.java
@@ -0,0 +1,77 @@
+package de.ulbms.scdh.seed.xc.resources.filesystem;
+
+import de.ulbms.scdh.seed.xc.api.ResourceException;
+import de.ulbms.scdh.seed.xc.api.ResourceInContext;
+import de.ulbms.scdh.seed.xc.api.ResourceNotFoundException;
+import de.ulbms.scdh.seed.xc.api.ResourceProvider;
+import de.ulbms.scdh.seed.xc.api.ResourceProviderConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A {@link ResourceProvider} that provides access to resources on the
+ * local file system.<P>
+ *
+ * Resource identifiers are interpreted as file paths. Relative paths
+ * are resolved against the path in the <code>path</code>
+ * configuration parameter. Absolute paths are interpreted as absolute
+ * paths.<P>
+ *
+ * Security: Only file paths under the configured <code>path</code>
+ * are accessible. For path to the outside, an exception is thrown.
+ */
+public class FileSystemResourceProvider implements ResourceProvider {
+
+	private static final Logger LOG =
+		LoggerFactory.getLogger(FileSystemResourceProvider.class);
+
+	private URI path;
+
+	private Exception error = null;
+
+	public FileSystemResourceProvider(
+		@ConfigProperty(name = "de.ulbms.scdh.seed.xc.resources.filesystem."
+							   + "FileSystemResourceProvider.path",
+						defaultValue = "/") String path) {
+		try {
+			this.path = Paths.get(new URI(path).normalize()).toUri();
+		} catch (URISyntaxException e) {
+			LOG.error("invalid URI (path) for FileSystemResourceProvider: {}",
+					  e.getMessage());
+			error = e;
+		} catch (NullPointerException e) {
+			LOG.error("invalid URI (path) for FileSystemResourceProvider: {}",
+					  e.getMessage());
+			error = e;
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public InputStream getSource(ResourceInContext ric)
+		throws ResourceProviderConfigurationException,
+			   ResourceNotFoundException, ResourceException {
+		if (error != null)
+			throw new ResourceProviderConfigurationException(error);
+		try {
+			URI normalized = path.resolve(ric.getResource()).normalize();
+			if (!normalized.toString().startsWith(path.toString())) {
+				throw new ResourceException("not allowed");
+			}
+			return normalized.toURL().openStream();
+		} catch (MalformedURLException e) {
+			throw new ResourceNotFoundException(e);
+		} catch (IOException e) {
+			throw new ResourceNotFoundException(e);
+		}
+	}
+}
