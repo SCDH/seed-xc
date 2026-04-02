858b3ec8ec9f4c3e9d148c6deb23ca402a14f72c
Christian Lück <christian.lueck@uni-muenster.de>
2026-04-02 14:46:48 +0200

    adds more tests for the navigation endpoint
    Signed-off-by: Christian Lück <christian.lueck@uni-muenster.de>

--- dts/src/test/java/de/ulbms/scdh/seed/xc/dts/navigation/NavigationEndpointTest.java
+++ dts/src/test/java/de/ulbms/scdh/seed/xc/dts/navigation/NavigationEndpointTest.java
@@ -1,10 +1,13 @@
 package de.ulbms.scdh.seed.xc.dts.navigation;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.collection.ArrayMatching.*;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
+import de.ulbms.scdh.seed.xc.dts.model.CitableUnit;
+import de.ulbms.scdh.seed.xc.dts.model.Navigation;
 import io.quarkus.test.junit.QuarkusTest;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
@@ -21,10 +24,104 @@ public class NavigationEndpointTest {
 	}
 
 	@Test
-	public void testHelloXml() {
+	public void testJohnXml200() {
 		given().when()
 				.get("/navigation?collection=file%3A%2F%2Fasdfasd%2F&down=0&" + "resource=john.xml")
 				.then()
 				.statusCode(200);
+	}
+
+	@Test
+	public void testJohnXmlResponseBodyHasLODContext() {
+		given().when()
+				.get("/navigation?collection=file%3A%2F%2Fasdfasd%2F&down=0&resource=john.xml")
+				.then()
+				.statusCode(200)
+				.body("@context", notNullValue());
+	}
+
+	@Test
+	public void testJohnXmlResponseBodyIsNavigationObject() {
+		given().when()
+				.get("/navigation?collection=file%3A%2F%2Fasdfasd%2F&down=0&resource=john.xml")
+				.then()
+				.extract()
+				.body()
+				.as(Navigation.class);
+	}
+
+	@Test
+	public void testJohnXmlResponseBodyHasCitationTrees() {
+		Navigation body = given().when()
+				.get("/navigation?collection=file%3A%2F%2Fasdfasd%2F&down=0&resource=john.xml")
+				.then()
+				.statusCode(200)
+				.extract()
+				.body()
+				.as(Navigation.class);
+		assertThat(body.getResource().getCitationTrees().size()).isEqualTo(8);
+		// default has no name
+		assertThat(body.getResource().getCitationTrees().get(0).getIdentifier()).isNull();
+		assertThat(body.getResource().getCitationTrees().get(1).getIdentifier()).isEqualTo("wadm");
+	}
+
+	@Test
+	public void testJohnXmlResponseBodyDefaultTreeMembers() {
+		Navigation body = given().when()
+				.get("/navigation?collection=file%3A%2F%2Fasdfasd%2F&resource=john.xml")
+				.then()
+				.statusCode(200)
+				.extract()
+				.body()
+				.as(Navigation.class);
+		// no members because tree not selected
+		assertThat(body.getMember().size()).isEqualTo(9);
+		assertThat(body.getMember().stream().map(CitableUnit::getCiteType).toList())
+				.isEqualTo(List.of("book", "chapter", "verse", "verse", "verse", "verse", "verse", "verse", "verse"));
+	}
+
+	@Test
+	public void testJohnXmlResponseBodyDown0Members() {
+		Navigation body = given().when()
+				.get("/navigation?collection=file%3A%2F%2Fasdfasd%2F&resource=john.xml&down=0")
+				.then()
+				.statusCode(200)
+				.extract()
+				.body()
+				.as(Navigation.class);
+		// no members because tree not selected
+		assertThat(body.getMember().size()).isEqualTo(1);
+		assertThat(body.getMember().stream().map(CitableUnit::getCiteType).toList())
+				.isEqualTo(List.of("book"));
+	}
+
+	@Test
+	public void testJohnXmlResponseBodyDown1Members() {
+		Navigation body = given().when()
+				.get("/navigation?collection=file%3A%2F%2Fasdfasd%2F&resource=john.xml&down=1")
+				.then()
+				.statusCode(200)
+				.extract()
+				.body()
+				.as(Navigation.class);
+		// no members because tree not selected
+		assertThat(body.getMember().size()).isEqualTo(1);
+		assertThat(body.getMember().stream().map(CitableUnit::getCiteType).toList())
+				.isEqualTo(List.of("book"));
+	}
+
+	@Test
+	public void testJohnXmlResponseBodyDown2Members() {
+		Navigation body = given().when()
+				.get("/navigation?collection=file%3A%2F%2Fasdfasd%2F&resource=john.xml&down=2")
+				.then()
+				.statusCode(200)
+				.extract()
+				.body()
+				.as(Navigation.class);
+		// no members because tree not selected
+		assertThat(body.getMember().size()).isEqualTo(2);
+		assertThat(body.getMember().stream().map(CitableUnit::getCiteType).toList())
+				.isEqualTo(List.of("book", "chapter"));
 	}
 }
