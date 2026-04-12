7f78de9506a867d516a518e118b99a638fd12a92
Christian Lück <christian.lueck@uni-muenster.de>
2026-04-12 23:30:55 +0200

    adds doc string
    Signed-off-by: Christian Lück <christian.lueck@uni-muenster.de>

--- api/src/main/java/de/ulbms/scdh/seed/xc/api/TransformationExceptionParser.java
+++ api/src/main/java/de/ulbms/scdh/seed/xc/api/TransformationExceptionParser.java
@@ -1,5 +1,10 @@
 package de.ulbms.scdh.seed.xc.api;
 
+/**
+ * A {@link TransformationExceptionParser} makes HTTP error
+ * codes and informative messages from high-level
+ * {@link TransformationException} exceptions.
+ */
 public interface TransformationExceptionParser {
 
 	/**
