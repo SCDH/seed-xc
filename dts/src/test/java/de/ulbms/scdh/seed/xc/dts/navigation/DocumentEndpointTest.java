d8bbde8fdc30da9f9eb08f1c7b3867ae030c06dd
Christian Lück <christian.lueck@uni-muenster.de>
2026-04-15 07:00:44 +0200

    adds some unit tests for document endpoint
    Signed-off-by: Christian Lück <christian.lueck@uni-muenster.de>

---
+++ dts/src/test/java/de/ulbms/scdh/seed/xc/dts/navigation/DocumentEndpointTest.java
@@ -0,0 +1,41 @@
+package de.ulbms.scdh.seed.xc.dts.navigation;
+
+import static io.restassured.RestAssured.given;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class DocumentEndpointTest {
+
+	@Test
+	public void testNoParams() {
+		given().when().get("/document").then().statusCode(400);
+	}
+
+	@Test
+	public void testOnlyCollection() {
+		given().when().get("/document/asdf").then().statusCode(404);
+	}
+
+	@Test
+	public void testJohnXml200() {
+		given().when().get("/document?resource=john.xml").then().statusCode(200);
+	}
+
+	@Test
+	public void testJohnXmlStartEnd() {
+		given().when()
+				.get("/document?resource=john.xml&start=eins&end=zwei")
+				.then()
+				.statusCode(500);
+	}
+
+	// TODO
+	@Disabled
+	@Test
+	public void testJohnXmlRef() {
+		given().when().get("/document?resource=john.xml&start=eins").then().statusCode(500);
+	}
+}
