605238e6a0a86d65839f88369f65ab5b66b79083
Christian Lück <christian.lueck@uni-muenster.de>
2026-06-21 21:46:22 +0200

    adds test for parametrized queries

--- dts/src/test/java/de/ulbms/scdh/seed/xc/dts/v1_0/CollectionEndpointTest.java
+++ dts/src/test/java/de/ulbms/scdh/seed/xc/dts/v1_0/CollectionEndpointTest.java
@@ -1,8 +1,15 @@
 package de.ulbms.scdh.seed.xc.dts.v1_0;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
 
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
@@ -10,6 +17,18 @@ public class CollectionEndpointTest {
 
 	@Test
 	public void testStatusGeneral() {
-		given().when().get("/collection?id=general").then().statusCode(200);
+		given().when().get("/collection?id=http://example.com/general").then().statusCode(200);
+	}
+
+	@TestHTTPEndpoint(CollectionEndpoint.class)
+	@TestHTTPResource("?id=general")
+	URL urlGeneral;
+
+	@Test
+	public void testGeneral() throws IOException {
+		try (InputStream in = urlGeneral.openStream()) {
+			String contents = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+			assertEquals("", contents);
+		}
 	}
 }
