91f55a78b0e27850e789b64437b2540bb87c8d4c
Christian Lück <christian.lueck@uni-muenster.de>
2026-06-22 09:13:55 +0200

    adds test for collection endpoint
    This is related to #15.

--- dts/src/test/java/de/ulbms/scdh/seed/xc/dts/v1_0/CollectionEndpointTest.java
+++ dts/src/test/java/de/ulbms/scdh/seed/xc/dts/v1_0/CollectionEndpointTest.java
@@ -6,14 +6,24 @@ import static org.junit.jupiter.api.Assertions.*;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.json.*;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class CollectionEndpointTest {
+
+	private static final String[] GENERAL_MEMBERS = {
+		"\"http://example.com/agrapha\"",
+		"\"http://example.com/apocryphs\"",
+		"\"http://example.com/john.xml\"",
+		"\"http://example.com/matt.xml\"",
+	};
 
 	@Test
 	public void testStatusGeneral() {
@@ -21,14 +31,35 @@ public class CollectionEndpointTest {
 	}
 
 	@TestHTTPEndpoint(CollectionEndpoint.class)
-	@TestHTTPResource("?id=general")
+	@TestHTTPResource("?id=http://example.com/general")
 	URL urlGeneral;
 
 	@Test
 	public void testGeneral() throws IOException {
 		try (InputStream in = urlGeneral.openStream()) {
-			String contents = new String(in.readAllBytes(), StandardCharsets.UTF_8);
-			assertEquals("", contents);
+			String result = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+			JsonReader reader = Json.createReader(new StringReader(result));
+			JsonStructure body = reader.read();
+			assertEquals(JsonValue.ValueType.OBJECT, body.getValueType());
+			JsonObject bodyObj = (JsonObject) body;
+			assertFalse(bodyObj.containsKey("@graph"), "has no @graph");
+			assertTrue(bodyObj.containsKey("@context"), "has @context");
+			assertEquals(10, bodyObj.size());
+			assertTrue(bodyObj.containsKey("@id"), "has @id");
+			assertEquals(
+					JsonValue.ValueType.STRING, ((JsonObject) body).get("@id").getValueType());
+			assertEquals("\"http://example.com/general\"", bodyObj.get("@id").toString());
+			assertTrue(bodyObj.containsKey("member"));
+			assertEquals(JsonValue.ValueType.ARRAY, bodyObj.get("member").getValueType());
+			JsonArray members = (JsonArray) bodyObj.get("member");
+			assertEquals(4, members.size());
+			members.forEach((m) -> {
+				assertEquals(JsonValue.ValueType.OBJECT, m.getValueType());
+				JsonObject member = (JsonObject) m;
+				String memberId = member.get("@id").toString();
+				assertTrue(Arrays.asList(GENERAL_MEMBERS).contains(memberId), memberId + " in members");
+			});
+			// assertEquals("", result);
 		}
 	}
 }
