43d0fa09b48c30b54cabde646d2a0aa103e014a4
Christian Lück <christian.lueck@uni-muenster.de>
2026-07-07 21:58:44 +0200

    introduces a resource provider manager or factory
    This is related to #34.

---
+++ api/src/main/java/de/ulbms/scdh/seed/xc/api/ResourceProviderManager.java
@@ -0,0 +1,18 @@
+package de.ulbms.scdh.seed.xc.api;
+
+import java.net.URI;
+
+/**
+ * A {@link ResourceProviderManager} is a bean that provides access to {@link ResourceProvider}s.
+ */
+public interface ResourceProviderManager {
+
+	/**
+	 * Returns a {@link ResourceProvider} for an identifier, e.g.
+	 * <code>urn</code>, and a base {@link URI}.
+	 * @param id - the identifier or type
+	 * @param base - the base URI
+	 * @return a resource provider
+	 */
+	ResourceProvider get(String id, URI base);
+}
