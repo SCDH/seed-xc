e29efef4bc9d4b5f78196bb7f942f102331b4613
Christian Lück <christian.lueck@uni-muenster.de>
2026-07-11 21:35:07 +0200

    registers missing OpenAPI models for reflection, closes #40
    new log from the startup:
    
    ```
    026-07-11 21:33:11,518 INFO  [de.ulbms.scdh.seed.xc.saxon.ConfiguredProcessor] (main) configured static URI resolver: null
    2026-07-11 21:33:11,518 INFO  [de.ulbms.scdh.seed.xc.saxon.ConfiguredProcessor] (main) configured output URI resolver: net.sf.saxon.lib.StandardOutputResolver
    2026-07-11 21:33:11,518 INFO  [de.ulbms.scdh.seed.xc.saxon.harden.RestrictiveResourceResolver] (main) allowed path of FileURIResolver configured to '/home/clueck/src/scdh/tei-processing/seed-xc/dts/../'
    2026-07-11 21:33:11,518 INFO  [de.ulbms.scdh.seed.xc.saxon.harden.RestrictiveResourceResolver] (main) allowed path of FileURIResolver set to '/home/clueck/src/scdh/tei-processing/seed-xc/'
    2026-07-11 21:33:11,529 INFO  [de.ulbms.scdh.seed.xc.transformations.ConfiguredTransformationMap] (main) creating transformation 'dts-transformations-rq-parents' ...
    2026-07-11 21:33:11,529 INFO  [de.ulbms.scdh.seed.xc.jena.StaticDocumentLoader] (main) using context map file:/home/clueck/src/scdh/tei-processing/seed-xc/dts/target/dependencies/dts-transformations/context-map.json
    2026-07-11 21:33:11,529 INFO  [de.ulbms.scdh.seed.xc.jena.StaticDocumentLoader] (main) using static asset xsl/context/modules/dct-nest.json instead of remote https://raw.githubusercontent.com/SCDH/dts-transformations/refs/tags/0.5.8/xsl/context/modules/dct-nest.json
    2026-07-11 21:33:11,529 INFO  [de.ulbms.scdh.seed.xc.jena.StaticDocumentLoader] (main) using static asset xsl/context/modules/dct-obj.json instead of remote https://scdh.github.io/dts-transformations/latest/xsl/context/modules/dct-obj.json
    2026-07-11 21:33:11,529 INFO  [de.ulbms.scdh.seed.xc.jena.StaticDocumentLoader] (main) using static asset xsl/context/modules/dct-obj.json instead of remote https://raw.githubusercontent.com/SCDH/dts-transformations/refs/tags/0.5.8/xsl/context/modules/dct-obj.json
    2026-07-11 21:33:11,529 INFO  [de.ulbms.scdh.seed.xc.jena.StaticDocumentLoader] (main) using static asset xsl/context/modules/dct-nest.json instead of remote https://scdh.github.io/dts-transformations/latest/xsl/context/modules/dct-nest.json
    2026-07-11 21:33:11,529 INFO  [de.ulbms.scdh.seed.xc.jena.StaticDocumentLoader] (main) using static asset xsl/context/1.0rc1.json instead of remote https://dtsapi.org/specifications/context/1.0rc1.json
    2026-07-11 21:33:11,529 INFO  [de.ulbms.scdh.seed.xc.jena.StaticDocumentLoader] (main) using static asset xsl/context/1.0rc1.json instead of remote https://raw.githubusercontent.com/SCDH/dts-transformations/refs/heads/main/xsl/context/1.0rc1.json
    2026-07-11 21:33:11,529 INFO  [de.ulbms.scdh.seed.xc.jena.StaticDocumentLoader] (main) using static asset xsl/context/modules/1.0rc1.json instead of remote https://raw.githubusercontent.com/SCDH/dts-transformations/refs/tags/0.5.8/xsl/context/modules/1.0rc1.json
    2026-07-11 21:33:11,529 INFO  [de.ulbms.scdh.seed.xc.jena.StaticDocumentLoader] (main) using static asset xsl/context/modules/1.0rc1.json instead of remote https://scdh.github.io/dts-transformations/latest/xsl/context/modules/1.0rc1.json
    2026-07-11 21:33:11,529 INFO  [de.ulbms.scdh.seed.xc.jena.StaticDocumentLoader] (main) using static asset xsl/context/modules/dct-nest.json instead of remote https://raw.githubusercontent.com/SCDH/dts-transformations/refs/heads/main/xsl/context/modules/dct-nest.json
    2026-07-11 21:33:11,529 INFO  [de.ulbms.scdh.seed.xc.jena.StaticDocumentLoader] (main) using static asset xsl/context/modules/dct-obj.json instead of remote https://raw.githubusercontent.com/SCDH/dts-transformations/refs/heads/main/xsl/context/modules/dct-obj.json
    2026-07- ...
    ``
    
    Signed-off-by: Christian Lück <christian.lueck@uni-muenster.de>

--- api/src/main/java/de/ulbms/scdh/seed/xc/api/ReflectionConfiguration.java
+++ api/src/main/java/de/ulbms/scdh/seed/xc/api/ReflectionConfiguration.java
@@ -5,22 +5,30 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 /**
  * A reflection configuration like described in <a
  * href="https://quarkus.io/guides/writing-native-applications-tips#registering-for-reflection">Tips
- * for writing native applications</a> by Quarkus.io.
+ * for writing native applications</a> by Quarkus.io.<P/>
  *
  * We have to register the Jackson data bindings in the generated
  * model classes, since they use reflection.
  */
 @RegisterForReflection(
 		targets = {
+			Config.class,
+			Context.class,
 			ParameterDescriptor.class,
+			ParameterValue.class,
+			Parser.class,
+			ResourceMapping.class,
+			ResourceMappingValue.class,
 			RuntimeParametersInitialCallValue.class,
 			RuntimeParameters.class,
+			Serializer.class,
 			TransformationIDs.class,
 			TransformationInfoInitialCallablesValue.class,
 			TransformationInfo.class,
 			TransformationInfoLibrariesInner.class,
 			TransformationMap.class,
+			TypedParameter.class,
 			XsltParameterDetails.class,
-			XsltParameterDetailsValue.class
+			XsltParameterDetailsValue.class,
 		})
 public class ReflectionConfiguration {}
