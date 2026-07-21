82a949ab60c840f6a509357a6a50b599749d03fd
Christian Lück <christian.lueck@uni-muenster.de>
2026-07-21 22:16:28 +0200

    workaround since EntityPart is not yet supported
    This is related to #55.
    
    EntityPart is not yet supported: https://github.com/quarkusio/quarkus/issues/41186
    
    Consider to contribute!
    
    Old RESTeasy annotations do not show any effect in generated OpenAPI specs or Swagger UI.

--- dts/src/main/java/de/ulbms/scdh/seed/xc/dts/v1_0/StandoffEndpoint.java
+++ dts/src/main/java/de/ulbms/scdh/seed/xc/dts/v1_0/StandoffEndpoint.java
@@ -3,11 +3,16 @@ package de.ulbms.scdh.seed.xc.dts.v1_0;
 import de.ulbms.scdh.seed.xc.dts.endpoints.StandoffApi;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.RequestScoped;
-import jakarta.ws.rs.core.EntityPart;
+import java.io.InputStream;
 import java.net.URI;
+import org.jboss.resteasy.reactive.PartType;
+import org.jboss.resteasy.reactive.RestForm;
 
 @RequestScoped
 public class StandoffEndpoint implements StandoffApi {
+
+	public static final String MEDIATYPE_ANNOTATIONS =
+			"application/ld+json;text/turtle;application/rdf+xml;application/n-triples;text/trig;application/n-quads;application/trix+xml;application/rdf+thrift;application/rdf+protobuf";
 
 	/**
 	 * {@inheritDoc}
@@ -17,13 +22,13 @@ public class StandoffEndpoint implements StandoffApi {
 			URI resource,
 			URI provider,
 			URI location,
-			EntityPart annotations,
+			@RestForm @PartType(MEDIATYPE_ANNOTATIONS) InputStream annotations,
 			String ref,
 			String start,
 			String end,
 			String tree,
 			String mediaType,
-			EntityPart frame) {
+			InputStream frame) {
 		return null;
 	}
 
@@ -35,13 +40,13 @@ public class StandoffEndpoint implements StandoffApi {
 			URI resource,
 			URI provider,
 			URI location,
-			EntityPart annotations,
+			@RestForm @PartType(MEDIATYPE_ANNOTATIONS) InputStream annotations,
 			String ref,
 			String start,
 			String end,
 			String tree,
 			String mediaType,
-			EntityPart frame) {
+			InputStream frame) {
 		return null;
 	}
 }
