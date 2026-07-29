f91d9eaccd0daf660cff139c7f1124bbd117de93
Christian Lück <christian.lueck@uni-muenster.de>
2026-07-29 18:13:20 +0200

    removed verbose logging
    This closes #57.

--- plugins/resource-providers/src/main/java/de/ulbms/scdh/seed/xc/resources/url/UrlResourceProviderBuilder.java
+++ plugins/resource-providers/src/main/java/de/ulbms/scdh/seed/xc/resources/url/UrlResourceProviderBuilder.java
@@ -4,8 +4,6 @@ import de.ulbms.scdh.seed.xc.api.*;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import java.net.URI;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * A {@link UrlResourceProviderBuilder} is a {@link ResourceProviderBuilder} that produces {@link UrlResourceProvider}
@@ -14,18 +12,8 @@ import org.slf4j.LoggerFactory;
 @ApplicationScoped
 public class UrlResourceProviderBuilder extends UrlValidator implements ResourceProviderBuilder {
 
-	private static final Logger LOG = LoggerFactory.getLogger(UrlResourceProviderBuilder.class);
-
 	@Inject
 	protected UrlConfig config;
-
-	public UrlResourceProviderBuilder() {}
-
-	@Inject
-	public UrlResourceProviderBuilder(UrlConfig config) {
-		LOG.info("used simplified injection to configure URL provider builder. {}", config.getAllowedProtocols());
-		this.config = config;
-	}
 
 	/**
 	 * {@inheritDoc}
@@ -41,14 +29,8 @@ public class UrlResourceProviderBuilder extends UrlValidator implements Resource
 	@Override
 	public ResourceProvider withBase(URI base)
 			throws ResourceException, ResourceProviderConfigurationException, ResourceNotFoundException {
-		LOG.info(
-				"going to configure URL provider with base {} and configuration {}",
-				base,
-				config.getAllowedProtocols());
 		configure(config);
 		check(base);
-		LOG.info("creating URL provider with base {} and configuration {}", base, config);
-		configure(config);
 		return new UrlResourceProvider(base, config);
 	}
 }
