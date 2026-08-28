9cba4339381e851fb2e61416e4f43b731e6506a2
Christian Lück <christian.lueck@uni-muenster.de>
2026-08-28 13:23:10 +0200

    try build native executable with nu.validator dep
    This is related to #68.

--- plugins/saxon/src/main/java/de/ulbms/scdh/seed/xc/saxon/ReflectionConfiguration.java
+++ plugins/saxon/src/main/java/de/ulbms/scdh/seed/xc/saxon/ReflectionConfiguration.java
@@ -1,7 +1,10 @@
 package de.ulbms.scdh.seed.xc.saxon;
 
+import com.ibm.icu.text.UnicodeSet;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import net.sf.saxon.Configuration;
+import nu.validator.htmlparser.extra.ChardetSniffer;
+import nu.validator.htmlparser.sax.HtmlParser;
 import org.apache.xerces.impl.dv.dtd.DTDDVFactoryImpl;
 import org.apache.xerces.parsers.XIncludeAwareParserConfiguration;
 import org.xmlresolver.loaders.XmlLoader;
@@ -14,6 +17,9 @@ import org.xmlresolver.loaders.XmlLoader;
 			Configuration.class,
 			XmlLoader.class,
 			XIncludeAwareParserConfiguration.class,
-			DTDDVFactoryImpl.class
+			DTDDVFactoryImpl.class,
+			HtmlParser.class,
+			UnicodeSet.class,
+			ChardetSniffer.class
 		})
 public class ReflectionConfiguration {}
